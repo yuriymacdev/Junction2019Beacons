811246e47663dbfa6c3a4ac8bff15c2434b4fd7f
Yuriy <john@MacBook.local>
2019-11-16 21:54:59 +0200

    Working http

--- frontend/Chernobyl/app/src/main/java/com/example/chernobyl/MainActivity.java
+++ frontend/Chernobyl/app/src/main/java/com/example/chernobyl/MainActivity.java
@@ -12,18 +12,27 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
+import android.icu.util.Output;
 import android.media.MediaPlayer;
 import android.media.PlaybackParams;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.RemoteException;
+import android.preference.PreferenceActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.ResponseHandlerInterface;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -32,11 +41,20 @@ import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.util.Collection;
 import java.util.Iterator;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
 
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
@@ -75,12 +93,176 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
             Log.e(TAG, "Unable to play audio queue do to exception: " + e.getMessage(), e);
         }
     }
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+
+
+
+    private void requestData(String url, String jsonRequestString) {
+        //Everything below is part of the Android Asynchronous HTTP Client
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.addHeader("content-type", "application/json");
+        RequestParams params = new RequestParams();
+        params.add("JSON", jsonRequestString);
+
+
+
+        client.post(url, params, new ResponseHandlerInterface() {
+            @Override
+            public void sendResponseMessage(HttpResponse response) throws IOException {
+                Log.i("sendResponseMessage", response.toString());
+                String s = convertStreamToString(response.getEntity().getContent());
+                Log.i("", s);
+
+            }
+
+            @Override
+            public void sendStartMessage() {
+
+            }
+
+            @Override
+            public void sendFinishMessage() {
+
+            }
+
+            @Override
+            public void sendProgressMessage(long bytesWritten, long bytesTotal) {
+
+            }
+
+            @Override
+            public void sendCancelMessage() {
+
+            }
+
+            @Override
+            public void sendSuccessMessage(int statusCode, Header[] headers, byte[] responseBody) {
+                Log.i("HTTP REQUEST RESPONSE", responseBody.toString());
+            }
+
+            @Override
+            public void sendFailureMessage(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.i("sendFailureMessage", ""+String.valueOf(statusCode));
+            }
+
+            @Override
+            public void sendRetryMessage(int retryNo) {
+
+            }
+
+            @Override
+            public URI getRequestURI() {
+                return null;
+            }
+
+            @Override
+            public void setRequestURI(URI requestURI) {
+
+            }
+
+            @Override
+            public Header[] getRequestHeaders() {
+                return new Header[0];
+            }
+
+            @Override
+            public void setRequestHeaders(Header[] requestHeaders) {
+
+            }
+
+            @Override
+            public boolean getUseSynchronousMode() {
+                return false;
+            }
+
+            @Override
+            public void setUseSynchronousMode(boolean useSynchronousMode) {
+
+            }
+
+            @Override
+            public boolean getUsePoolThread() {
+                return false;
+            }
+
+            @Override
+            public void setUsePoolThread(boolean usePoolThread) {
+
+            }
+
+            @Override
+            public void onPreProcessResponse(ResponseHandlerInterface instance, HttpResponse response) {
+
+            }
+
+            @Override
+            public void onPostProcessResponse(ResponseHandlerInterface instance, HttpResponse response) {
+
+            }
+
+            @Override
+            public Object getTag() {
+                return null;
+            }
+
+            @Override
+            public void setTag(Object TAG) {
+
+            }
+        });
+
+//        client.get(url, new JsonHttpResponseHandler() {
+//            @Override
+//            public void onSuccess(int statusCode, PreferenceActivity.Header[] headers,
+//                                  JSONObject response) {
+//                // called when response HTTP status is "200 OK"
+//                Log.d("Bitcoin", "JSON: " + response.toString());
+//
+//                try {
+//
+//                    //The “ask” value below is a field in the JSON Object that was
+//                    //retrieved from the BitcoinAverage API. It contains the current
+//                    //bitcoin price
+//
+//                    String price = response.getString("ask");
+//
+//                    //Now we can use the value in the mPriceTextView
+//
+//
+//                } catch (Exception e) {
+//
+//                    Log.e("Bitcoin", e.toString());
+//
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onFailure(int statusCode, Header[] headers, Throwable e,
+//                                  JSONObject response) {
+//
+//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+//
+//                Log.d("Bitcoin", "Request fail! Status code: " + statusCode);
+//                Log.d("Bitcoin", "Fail response: " + response);
+//                Log.e("ERROR", e.toString());
+//
+//                Toast.makeText(MainActivity.this, "Request Failed",
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        requestData("http://10.84.113.85:8000", "{\"what\": \"reset\"}");
 
         playSample(R.raw.ic_geiger);
 
@@ -180,6 +362,20 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
             }
         }.start();
 
+        ImageView firstImage=(ImageView) findViewById(R.id.firstPlayerImageView);
+        firstImage.setImageResource(R.drawable.ic_button1);
+
+        ImageView secondImage=(ImageView) findViewById(R.id.secondPlayerImageView);
+        secondImage.setImageResource(R.drawable.ic_button2);
+
+        TextView firstText=(TextView) findViewById(R.id.firstPlayerTextView);
+        firstText.setText("Mykola");
+
+        TextView secondText=(TextView) findViewById(R.id.secondPlayerTextView);
+        secondText.setText("Yuri");
+
+
+
     }
     @Override
     protected void onDestroy() {
@@ -204,7 +400,7 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
 
 
                         if(beacon.getBluetoothAddress().toString().equals("D2:D7:7E:14:19:00")) {
-                            TextView tv1 = (TextView)findViewById(R.id.testtextview);
+                            TextView tv1 = (TextView)findViewById(R.id.infoTextView);
                             Log.i("", "Our beacon found: " + String.valueOf(beacon.getRssi()));
 
 
